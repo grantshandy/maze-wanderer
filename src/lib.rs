fecbb7ae0b09cc7fa75306fd727cb20d65e9e182
grantshandy <grantshandy@gmail.com>
2022-11-18 17:24:02 -0700

    finish work on top down view and player controls

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
 #![no_std]
 
+use core::f32::consts::PI;
+
 use heapless::Vec;
+use libm::{cosf, sinf};
 
 mod maps;
 
 use maps::DEFAULT_MAP;
 
-// square screen size
+// env constants
 pub const SCREEN_SIZE: u16 = 640;
-
-// the maximum x / y size of the map
 pub const MAX_MAP_SIZE: usize = 32;
+const TWO_PI: f32 = PI * 2.0;
 
-const MOVEMENT_STEP: f32 = 0.25;
+// gameplay constants
+const MOVEMENT_STEP: f32 = 0.07;
+const LOOK_STEP: f32 = 0.07;
 
 // a simplified type that represents the in-memory map.
 type WorldMap = Vec<Vec<bool, MAX_MAP_SIZE>, MAX_MAP_SIZE>;
@@ -26,11 +30,13 @@ pub struct State {
 impl Default for State {
     fn default() -> Self {
         Self {
-            map: to_worldmap(DEFAULT_MAP),
+            map: const_to_worldmap(DEFAULT_MAP),
             player: Player {
-                pos_x: 5.5,
-                pos_y: 12.5,
-                direction: 0.0,
+                x: 5.5,
+                y: 12.5,
+                dx: MOVEMENT_STEP,
+                dy: 0.0,
+                angle: 0.0,
             },
         }
     }
@@ -62,22 +68,61 @@ impl State {
 
 #[derive(Clone, Copy)]
 pub struct Player {
-    pub pos_x: f32,
-    pub pos_y: f32,
-    pub direction: f32,
+    // position
+    pub x: f32,
+    pub y: f32,
+    // next place
+    pub dx: f32,
+    pub dy: f32,
+    // angle
+    pub angle: f32,
 }
 
 impl Player {
     pub fn move_forward(&mut self) {
-        self.pos_y += MOVEMENT_STEP;
+        self.new_loc(self.x + self.dx, self.y + self.dy);
     }
 
     pub fn move_backward(&mut self) {
-        self.pos_y -= MOVEMENT_STEP;
+        self.new_loc(self.x - self.dx, self.y - self.dy);
+    }
+
+    // created to reduce number of instructions generated
+    fn new_loc(&mut self, new_x: f32, new_y: f32) {
+        if (new_x < MAX_MAP_SIZE as f32 && new_x > 0.0)
+            && (new_y < MAX_MAP_SIZE as f32 && new_y > 0.0)
+        {
+            self.x = new_x;
+            self.y = new_y;
+        }
+    }
+
+    pub fn look_left(&mut self) {
+        self.angle -= LOOK_STEP;
+
+        self.calc_new_angle();
+    }
+
+    pub fn look_right(&mut self) {
+        self.angle += LOOK_STEP;
+        
+        self.calc_new_angle();
+    }
+    
+    // also created to reduce number of wasm instructions
+    fn calc_new_angle(&mut self) {
+        if self.angle < 0.0 {
+            self.angle = TWO_PI;
+        } else if self.angle > TWO_PI {
+            self.angle = 0.0;
+        }
+
+        self.dx = cosf(self.angle) * MOVEMENT_STEP;
+        self.dy = sinf(self.angle) * MOVEMENT_STEP;
     }
 }
 
-fn to_worldmap(raw: &[&[bool]]) -> WorldMap {
+fn const_to_worldmap(raw: &[&[bool]]) -> WorldMap {
     raw.iter()
         .map(|line| Vec::from_slice(line).unwrap())
         .collect()
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use macroquad::prelude::*;
 
 use raycaster::{self, State, MAX_MAP_SIZE, SCREEN_SIZE};
 
+// the factor for the size of the top down cells on screen
+const TOP_DOWN_CELL_SIZE: usize = SCREEN_SIZE as usize / MAX_MAP_SIZE;
+
+// length factor to visualize where the player is looking from dx/dy
+const VIEW_LINE_LENGTH_FACTOR: f32 = 300.0;
+
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Raycaster Demo".to_owned(),
@@ -16,39 +23,12 @@ fn window_conf() -> Conf {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut state = State::default();
-    let cell_size: usize = SCREEN_SIZE as usize / MAX_MAP_SIZE;
 
     loop {
-        // draw map
-        for (y, values) in state.map().iter().enumerate() {
-            let y_adj: f32 = (y * cell_size) as f32;
+        draw_top_down(&state);
+        draw_pov(&state);
 
-            for (x, value) in values.iter().enumerate() {
-                let x_adj: f32 = (x * cell_size) as f32;
-
-                let color = if *value { DARKBLUE } else { GRAY };
-
-                draw_rectangle(x_adj, y_adj, cell_size as f32, cell_size as f32, color);
-            }
-        }
-
-        // draw grid lines
-        for x in 0..(MAX_MAP_SIZE + 1) {
-            let x: f32 = (x * cell_size) as f32;
-            draw_line(x, 0.0, x, SCREEN_SIZE as f32, 1.0, DARKGRAY);
-            draw_line(0.0, x, SCREEN_SIZE as f32, x, 1.0, DARKGRAY);
-        }
-
-        // draw player
-        let player = state.player();
-        draw_circle(
-            player.pos_x * cell_size as f32,
-            player.pos_y * cell_size as f32,
-            4.3,
-            RED,
-        );
-
-        // move player
+        // move player around a bit
         if is_key_down(KeyCode::W) {
             state.player_mut().move_forward();
         }
@@ -58,11 +38,67 @@ async fn main() {
         }
 
         if is_key_down(KeyCode::A) {
+            state.player_mut().look_left();
         }
 
         if is_key_down(KeyCode::D) {
+            state.player_mut().look_right();
         }
 
         next_frame().await;
     }
+}
+
+// pov is drawn on the right so everything needs to be shifted
+const SCREEN_OFFSET: f32 = SCREEN_SIZE as f32;
+
+fn draw_pov(state: &State) {
+}
+
+fn draw_top_down(state: &State) {
+    // draw map cells
+    for (y, values) in state.map().iter().enumerate() {
+        let y_adj: f32 = (y * TOP_DOWN_CELL_SIZE) as f32;
+
+        for (x, value) in values.iter().enumerate() {
+            let x_adj: f32 = (x * TOP_DOWN_CELL_SIZE) as f32;
+
+            let color = if *value { DARKBLUE } else { GRAY };
+
+            draw_rectangle(
+                x_adj,
+                y_adj,
+                TOP_DOWN_CELL_SIZE as f32,
+                TOP_DOWN_CELL_SIZE as f32,
+                color,
+            );
+        }
+    }
+
+    // draw grid lines
+    for x in 0..(MAX_MAP_SIZE + 1) {
+        let x: f32 = (x * TOP_DOWN_CELL_SIZE) as f32;
+        draw_line(x, 0.0, x, SCREEN_SIZE as f32, 1.0, DARKGRAY);
+        draw_line(0.0, x, SCREEN_SIZE as f32, x, 1.0, DARKGRAY);
+    }
+
+    // draw player
+    let player = state.player();
+    let (adj_player_x, adj_player_y) = (
+        player.x * TOP_DOWN_CELL_SIZE as f32,
+        player.y * TOP_DOWN_CELL_SIZE as f32,
+    );
+
+    // draw view line
+    draw_line(
+        adj_player_x,
+        adj_player_y,
+        adj_player_x + (player.dx * VIEW_LINE_LENGTH_FACTOR),
+        adj_player_y + (player.dy * VIEW_LINE_LENGTH_FACTOR),
+        2.0,
+        YELLOW,
+    );
+
+    // draw player circle
+    draw_circle(adj_player_x, adj_player_y, 4.3, RED);
 }

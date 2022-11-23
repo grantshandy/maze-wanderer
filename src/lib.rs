011a189ece5a6fd5fc50962dd0c9fc4207e8e652
grantshandy <grantshandy@gmail.com>
2022-11-22 17:32:06 -0700

    getting closer

---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+#![no_std]
+
+use core::f32::consts::{PI, TAU};
+
+use libm::{cosf, sinf};
+
+pub const SCREEN_SIZE: i32 = 160;
+
+pub const FOV: f32 = PI / 3.0;
+pub const HALF_FOV: f32 = FOV / 2.0;
+
+pub const MAP_SIZE: i32 = 8;
+const MAP_BUFFER: usize = MAP_SIZE as usize * MAP_SIZE as usize;
+
+const MOVE_STEP: f32 = 0.05;
+const LOOK_STEP: f32 = 0.1;
+
+#[rustfmt::skip]
+const DEFAULT_MAP: [bool; MAP_BUFFER] = [
+    true, true, true, true, true, true, true, true,
+    true, false, false, false, true, true, false, true,
+    true, false, false, false, false, false, false, true,
+    true, false, false, false, false, true, true, true,
+    true, true, false, false, false, false, false, true,
+    true, false, false, false, true, false, false, true,
+    true, false, false, false, true, false, false, true,
+    true, true, true, true, true, true, true, true
+];
+
+pub struct State {
+    pub player_x: f32,
+    pub player_y: f32,
+    pub player_angle: f32,
+    pub player_forward: bool,
+    pub map: [bool; MAP_BUFFER],
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self {
+            player_x: 3.5,
+            player_y: 3.5,
+            player_angle: PI,
+            player_forward: true,
+            map: DEFAULT_MAP,
+        }
+    }
+}
+
+impl State {
+    // update the player's movement
+    pub fn update_character(&mut self, left: bool, right: bool, forwards: bool, backwards: bool) {
+        if left {
+            self.player_angle -= LOOK_STEP;
+        }
+
+        if right {
+            self.player_angle += LOOK_STEP;
+        }
+
+        if forwards {
+            self.player_forward = true;
+            self.player_x += -sinf(self.player_angle) * MOVE_STEP;
+            self.player_y += cosf(self.player_angle) * MOVE_STEP;
+        }
+
+        if backwards {
+            self.player_forward = false;
+            self.player_x -= -sinf(self.player_angle) * MOVE_STEP;
+            self.player_y -= cosf(self.player_angle) * MOVE_STEP;
+        }
+
+        if self.map[(self.player_y as i32 * MAP_SIZE + self.player_x as i32) as usize] {
+            if self.player_forward {
+                self.player_x -= -sinf(self.player_angle) * MOVE_STEP;
+                self.player_y -= cosf(self.player_angle) * MOVE_STEP;
+            } else {
+                self.player_x += -sinf(self.player_angle) * MOVE_STEP;
+                self.player_y += cosf(self.player_angle) * MOVE_STEP;
+            }
+        }
+
+        if self.player_angle > TAU {
+            self.player_angle = 0.0;
+        } else if self.player_angle < 0.0 {
+            self.player_angle = TAU;
+        }
+    }
+
+    pub fn raycast(&self) -> (f32, f32) {
+        let start_angle = self.player_angle;
+
+        for depth in 0..MAP_SIZE {
+            let target_x = self.player_x - sinf(start_angle) * depth as f32;
+            let target_y = self.player_y + cosf(start_angle) * depth as f32;
+
+            let col: i32 = target_x as i32;
+            let row: i32 = target_y as i32;
+
+            let square: i32 = (row * MAP_SIZE) + col;
+
+            if self.map[square as usize] {
+                return (target_x, target_y);
+            }
+        }
+
+        (0.0, 0.0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
-use std::f32::consts::PI;
-
 use libm::{cosf, sinf};
 use macroquad::prelude::*;
+use raycaster::{State, HALF_FOV, MAP_SIZE, SCREEN_SIZE};
 
-const SCREEN_HEIGHT: i32 = 480;
-const SCREEN_WIDTH: i32 = SCREEN_HEIGHT * 2;
-const MAP_SIZE: i32 = 8;
-const TILE_SIZE: i32 = (SCREEN_WIDTH / 2) / MAP_SIZE;
-const MAX_DEPTH: i32 = MAP_SIZE * TILE_SIZE;
-const FOV: f32 = PI / 3.0;
-const HALF_FOV: f32 = FOV / 2.0;
-const CASTED_RAYS: i32 = 120;
-const STEP_ANGLE: f32 = FOV / CASTED_RAYS as f32;
-const SCALE: i32 = (SCREEN_WIDTH / 2) / CASTED_RAYS;
+// multiply inner resolution to look nice for development
+const DISPLAY_SCALE: i32 = 4;
+pub const TILE_SIZE: i32 = SCREEN_SIZE / MAP_SIZE;
 
-const MAP: &str = "#########   ## ##      ##    #####     ##   #  ##   #  #########";
+// colors
+const SKY: Vec4 = Vec4::new(0.243, 0.596, 0.737, 1.0);
+const GROUND: Vec4 = Vec4::new(0.6, 0.57, 0.45, 1.0);
 
 fn window_conf() -> Conf {
     Conf {
-        window_title: "Raycaster Demo".to_owned(),
-        window_width: SCREEN_WIDTH,
-        window_height: SCREEN_HEIGHT,
+        window_title: "Raycaster".to_string(),
+        window_width: (SCREEN_SIZE * 2) * DISPLAY_SCALE,
+        window_height: SCREEN_SIZE * DISPLAY_SCALE,
         window_resizable: false,
         ..Default::default()
     }
@@ -28,187 +22,106 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let mut player_x: f32 = ((SCREEN_WIDTH / 2) / 2) as f32;
-    let mut player_y: f32 = ((SCREEN_WIDTH / 2) / 2) as f32;
-    let mut player_angle: f32 = PI;
-    let mut forward = true;
+    let mut state = State::default();
 
     loop {
-        let col: i32 = player_x as i32 / TILE_SIZE;
-        let row: i32 = player_y as i32 / TILE_SIZE;
-
-        let square: i32 = (row * MAP_SIZE) + col;
-        println!("{square}");
-
-        if MAP.chars().nth(square as usize) == Some('#') {
-            if forward {
-                player_x -= -sinf(player_angle) * 5.0;
-                player_y -= cosf(player_angle) * 5.0;
-            } else {
-                player_x += -sinf(player_angle) * 5.0;
-                player_y += cosf(player_angle) * 5.0;
-            }
-        }
-
-        // draw background colors
-        draw_rectangle(
-            0.0,
-            0.0,
-            SCREEN_HEIGHT as f32,
-            SCREEN_HEIGHT as f32,
-            Color::from_rgba(0, 0, 0, 255),
-        );
-        draw_rectangle(
-            480.0,
-            (SCREEN_HEIGHT / 2) as f32,
-            SCREEN_HEIGHT as f32,
-            SCREEN_HEIGHT as f32,
-            Color::from_rgba(100, 0, 0, 255),
-        );
-        draw_rectangle(
-            480.0,
-            (-SCREEN_HEIGHT / 2) as f32,
-            SCREEN_HEIGHT as f32,
-            SCREEN_HEIGHT as f32,
-            Color::from_rgba(200, 0, 0, 255),
+        state.update_character(
+            is_key_down(KeyCode::Left),
+            is_key_down(KeyCode::Right),
+            is_key_down(KeyCode::Up),
+            is_key_down(KeyCode::Down),
         );
 
-        draw_map(player_x, player_y, player_angle);
-        cast_rays(player_x, player_y, player_angle);
+        draw_map(&state);
 
-        if is_key_down(KeyCode::Left) {
-            player_angle -= 0.1;
-        }
-
-        if is_key_down(KeyCode::Right) {
-            player_angle += 0.1;
-        }
-
-        if is_key_down(KeyCode::Up) {
-            forward = true;
-            player_x += -sinf(player_angle) * 5.0;
-            player_y += cosf(player_angle) * 5.0;
-        }
-
-        if is_key_down(KeyCode::Down) {
-            forward = false;
-            player_x -= -sinf(player_angle) * 5.0;
-            player_y -= cosf(player_angle) * 5.0;
-        }
-
-        draw_text(
-            &format!("{}", get_fps()),
-            1.0,
-            18.0,
-            30.0,
-            Color::from_rgba(255, 255, 255, 255),
-        );
+        // println!("{}", state.player_angle);
+        // println!(
+        //     "current square: {}",
+        //     (state.player_y as i32 * MAP_SIZE + state.player_x as i32)
+        // );
 
         next_frame().await;
     }
 }
 
-fn draw_map(player_x: f32, player_y: f32, player_angle: f32) {
+fn draw_map(state: &State) {
+    // draw background
+    draw_rectangle(
+        0.0,
+        0.0,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        BLACK,
+    );
+    draw_rectangle(
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        0.0,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32 / 2.0,
+        Color::from_vec(SKY),
+    );
+    draw_rectangle(
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32 / 2.0,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32,
+        (SCREEN_SIZE * DISPLAY_SCALE) as f32 / 2.0,
+        Color::from_vec(GROUND),
+    );
+
+    // draw grid
     for row in 0..MAP_SIZE {
         for col in 0..MAP_SIZE {
-            let square = row * MAP_SIZE + col;
-
-            if MAP.chars().nth(square as usize) == Some('#') {
-                draw_rectangle(
-                    (col * TILE_SIZE) as f32,
-                    (row * TILE_SIZE) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    Color::from_rgba(200, 200, 200, 255),
-                );
+            let grid_color = if state.map[(row * MAP_SIZE + col) as usize] {
+                Color::from_rgba(200, 200, 200, 255)
             } else {
-                draw_rectangle(
-                    (col * TILE_SIZE) as f32,
-                    (row * TILE_SIZE) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    Color::from_rgba(100, 100, 100, 255),
-                );
-            }
+                Color::from_rgba(100, 100, 100, 255)
+            };
+
+            draw_rectangle(
+                (col * TILE_SIZE * DISPLAY_SCALE) as f32,
+                (row * TILE_SIZE * DISPLAY_SCALE) as f32,
+                (DISPLAY_SCALE * TILE_SIZE - 2) as f32,
+                (DISPLAY_SCALE * TILE_SIZE - 2) as f32,
+                grid_color,
+            );
         }
     }
 
-    draw_circle(player_x, player_y, 8.0, Color::from_rgba(255, 0, 0, 255));
+    // draw direction indicators
     draw_line(
-        player_x,
-        player_y,
-        player_x - sinf(player_angle) * 50.0,
-        player_y + cosf(player_angle) * 50.0,
+        state.player_x * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        state.player_y * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        (state.player_x - sinf(state.player_angle - HALF_FOV)) * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        (state.player_y + cosf(state.player_angle - HALF_FOV)) * (DISPLAY_SCALE * TILE_SIZE) as f32,
         3.0,
-        Color::from_rgba(0, 255, 0, 255),
+        GREEN,
     );
     draw_line(
-        player_x,
-        player_y,
-        player_x - sinf(player_angle - HALF_FOV) * 50.0,
-        player_y + cosf(player_angle - HALF_FOV) * 50.0,
+        state.player_x * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        state.player_y * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        (state.player_x - sinf(state.player_angle + HALF_FOV)) * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        (state.player_y + cosf(state.player_angle + HALF_FOV)) * (DISPLAY_SCALE * TILE_SIZE) as f32,
         3.0,
-        Color::from_rgba(0, 255, 0, 255),
+        GREEN,
     );
+
+    // draw map ray from center of player's vision
+    let (target_x, target_y) = &state.raycast();
     draw_line(
-        player_x,
-        player_y,
-        player_x - sinf(player_angle + HALF_FOV) * 50.0,
-        player_y + cosf(player_angle + HALF_FOV) * 50.0,
-        3.0,
-        Color::from_rgba(0, 255, 0, 255),
+        state.player_x * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        state.player_y * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        target_x * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        target_y * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        4.0,
+        YELLOW,
     );
-}
+    println!("{:?}", (target_x, target_y));
 
-fn cast_rays(player_x: f32, player_y: f32, player_angle: f32) {
-    let mut start_angle: f32 = player_angle - HALF_FOV;
-
-    for ray in 0..CASTED_RAYS {
-        for depth in 0..MAX_DEPTH {
-            let target_x = player_x - sinf(start_angle) * depth as f32;
-            let target_y = player_y + cosf(start_angle) * depth as f32;
-
-            let col: i32 = target_x as i32 / TILE_SIZE;
-            let row: i32 = target_y as i32 / TILE_SIZE;
-
-            let square: i32 = (row * MAP_SIZE) + col;
-
-            if MAP.chars().nth(square as usize) == Some('#') {
-                draw_rectangle(
-                    (col * TILE_SIZE) as f32,
-                    (row * TILE_SIZE) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    (TILE_SIZE - 2) as f32,
-                    Color::from_rgba(0, 255, 0, 255),
-                );
-                draw_line(
-                    player_x,
-                    player_y,
-                    target_x,
-                    target_y,
-                    1.0,
-                    Color::from_rgba(255, 255, 0, 255),
-                );
-
-                let depth_alt = depth as f32 * cosf(player_angle - start_angle);
-                let mut wall_height = 21000.0 / (depth_alt + 0.0001);
-
-                if wall_height > SCREEN_HEIGHT as f32 {
-                    wall_height = SCREEN_HEIGHT as f32;
-                }
-
-                draw_rectangle(
-                    (SCREEN_HEIGHT + ray * SCALE) as f32,
-                    (SCREEN_HEIGHT / 2) as f32 - wall_height / 2.0,
-                    SCALE as f32,
-                    wall_height,
-                    Color::from_rgba(30, 30, 30, 255),
-                );
-
-                break;
-            }
-        }
-
-        start_angle += STEP_ANGLE;
-    }
+    // draw player
+    draw_circle(
+        state.player_x * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        state.player_y * (DISPLAY_SCALE * TILE_SIZE) as f32,
+        8.0,
+        RED,
+    );
 }
